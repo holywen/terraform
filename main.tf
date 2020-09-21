715240fbce47eda9609078861a103316ce0efe99
Shaohua Wen <shaohua.wen@gmail.com>
2020-09-21 12:08:16 +0800

    remove network creating

--- main.tf
+++ main.tf
@@ -13,7 +13,8 @@ resource "google_compute_instance" "vm_instance" {
   }
   network_interface {
     # A default network is created for all GCP projects
-    network = google_compute_network.vpc_network.self_link
+    network = default
+    #network = google_compute_network.vpc_network.self_link
     access_config {
     }
   }
